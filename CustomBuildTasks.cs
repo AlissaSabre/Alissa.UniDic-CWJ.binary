a51425a06d4e474a2a44d9d07a75593c298e8839
Alissa Sabre <alissa_sabre@yahoo.co.jp>
2020-06-02 22:22:26 +0900

    Unzip automatically during build.

--- CustomBuildTasks.cs
+++ CustomBuildTasks.cs
@@ -10,8 +10,14 @@ namespace Alissa.UniDic.CustomBuildTasks
     /// </summary>
     public class ConcatnateFiles : ITask
     {
+        /// <summary>
+        /// Implements <see cref="ITask.BuildEngine"/>
+        /// </summary>
         public IBuildEngine BuildEngine { get; set; }
 
+        /// <summary>
+        /// Implements <see cref="ITask.HostObject"/>
+        /// </summary>
         public ITaskHost HostObject { get; set; }
 
         /// <summary>
@@ -32,23 +38,17 @@ namespace Alissa.UniDic.CustomBuildTasks
         public ITaskItem Target { get; set; }
 
         /// <summary>
-        /// The main task action to concatenate files.
+        /// Implements <see cref="ITask.Execute"/> to concatenate files.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>True if successful.  False otherwise.</returns>
         public bool Execute()
         {
-            bool error = false;
+            bool success = true;
             try
             {
                 using (var output = File.OpenWrite(Target.ItemSpec))
                 {
-                    var sorted = new ITaskItem[Sources.Length];
-                    Array.Copy(Sources, sorted, Sources.Length);
-                    Array.Sort(sorted, (x, y) => string.Compare(
-                        Path.GetFileName(x.ItemSpec),
-                        Path.GetFileName(y.ItemSpec),
-                        StringComparison.InvariantCultureIgnoreCase));
-                    foreach (var source in sorted)
+                    foreach (var source in Helper.SortItems(Sources))
                     {
                         try
                         {
@@ -64,18 +64,18 @@ namespace Alissa.UniDic.CustomBuildTasks
                         {
                             // Log the error and keep going,
                             // so that any further issues are reported during this build.
-                            LogException(e);
-                            error = true;
+                            Helper.LogError(BuildEngine, e.Message);
+                            success = false;
                         }
                     }
                 }
             }
             catch (Exception e)
             {
-                LogException(e);
-                error = true;
+                Helper.LogError(BuildEngine, e.Message);
+                success = false ;
             }
-            if (error)
+            if (!success)
             {
                 // Try not to leave a partial file, because it will confuse MSBuild.
                 try
@@ -86,23 +86,159 @@ namespace Alissa.UniDic.CustomBuildTasks
                 {
                 }
             }
-            return error;
+            return success;
+        }
+    }
+
+    /// <summary>
+    /// An MSBuild task to split a single large file into mutltiple smaller parts.
+    /// </summary>
+    public class SplitFile : ITask
+    {
+        private const int BufferSize = 65536;
+
+        /// <summary>
+        /// Implements <see cref="ITask.BuildEngine"/>
+        /// </summary>
+        public IBuildEngine BuildEngine { get; set; }
+
+        /// <summary>
+        /// Implements <see cref="ITask.HostObject"/>
+        /// </summary>
+        public ITaskHost HostObject { get; set; }
+
+        /// <summary>
+        /// A file to split.
+        /// </summary>
+        [Required]
+        public ITaskItem Source { get; set; }
+
+        /// <summary>
+        /// A set of files to split the file into.
+        /// </summary>
+        /// <remarks>
+        /// At least one Target is required.
+        /// The files will be almost same size after the split.
+        /// 
+        /// </remarks>
+        [Required]
+        public ITaskItem[] Targets { get; set; }
+
+        /// <summary>
+        /// Implements <see cref="ITask.Execute"/> to concatenate files.
+        /// </summary>
+        /// <returns>True if successful.  False otherwise.</returns>
+        public bool Execute()
+        {
+            if (Targets.Length < 2)
+            {
+                Helper.LogError(BuildEngine, "At least two targets are needed.");
+                return false;
+            }
+
+            bool success = true;
+            try
+            {
+                using (var input = File.OpenRead(Source.ItemSpec))
+                {
+                    var targets = Helper.SortItems(Targets);
+                    long part_length = input.Length / targets.Length;
+                    int extras = (int)(input.Length % targets.Length);
+                    var buffer = new byte[BufferSize];
+                    for (int i = 0; i < targets.Length; i++)
+                    {
+                        long remaining = part_length + (i < extras ? 1 : 0);
+                        long goal_position = input.Position + remaining;
+                        try
+                        {
+                            using (var output = File.OpenWrite(targets[i].ItemSpec))
+                            {
+                                while (remaining > 0)
+                                {
+                                    int length = (int)Math.Min(remaining, buffer.Length);
+                                    int n = input.Read(buffer, 0, length);
+                                    output.Write(buffer, 0, n);
+                                    remaining -= n;
+                                }
+                            }
+                        }
+                        catch (IOException e)
+                        {
+                            // Log the error and keep going,
+                            // so that any further issues are reported during this build.
+                            Helper.LogError(BuildEngine, e.Message);
+                            success = false;
+
+                            // Try not to leave an incomplete target file.
+                            try
+                            {
+                                File.Delete(targets[i].ItemSpec);
+                            }
+                            catch (Exception)
+                            {
+                            }
+
+                            // Try to make contents of other files as expected;
+                            try
+                            {
+                                input.Position = goal_position;
+                            }
+                            catch (Exception)
+                            {
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Helper.LogError(BuildEngine, e.Message);
+                success = false;
+            }
+            return success;
+        }
+    }
+
+    /// <summary>
+    /// Provides helpful static methods.
+    /// </summary>
+    internal static class Helper
+    {
+        /// <summary>
+        /// Returns an array of items ordered in <see cref="ITaskItem.ItemSpec"/>.
+        /// </summary>
+        /// <param name="items">An array of items to sort.</param>
+        /// <returns>an array containing sorted items.</returns>
+        public static ITaskItem[] SortItems(ITaskItem[] items)
+        {
+            var sorted = new ITaskItem[items.Length];
+            Array.Copy(items, sorted, items.Length);
+            Array.Sort(sorted, (x, y) => string.Compare(
+                Path.GetFileName(x.ItemSpec),
+                Path.GetFileName(y.ItemSpec),
+                StringComparison.InvariantCultureIgnoreCase));
+            return sorted;
         }
 
-        private void LogException(Exception exception)
+        /// <summary>
+        /// Log an error event.
+        /// </summary>
+        /// <param name="engine">A build engine to log an error event.</param>
+        /// <param name="message">An error message to log.</param>
+        public static void LogError(IBuildEngine engine, string message)
         {
             var event_args = new BuildErrorEventArgs(
                 subcategory: null,
                 code: null,
-                file: BuildEngine.ProjectFileOfTaskNode,
-                lineNumber: BuildEngine.LineNumberOfTaskNode,
-                columnNumber: BuildEngine.ColumnNumberOfTaskNode,
+                file: engine.ProjectFileOfTaskNode,
+                lineNumber: engine.LineNumberOfTaskNode,
+                columnNumber: engine.ColumnNumberOfTaskNode,
                 endLineNumber: 0,
                 endColumnNumber: 0,
-                message: exception.Message,
+                message: message,
                 helpKeyword: null,
                 senderName: nameof(ConcatnateFiles));
-            BuildEngine.LogErrorEvent(event_args);
+            engine.LogErrorEvent(event_args);
         }
     }
 }
